5bb304be3a75dbf3a346f41a6d4d6ab7756c39f6
chellojuramu <chelloju@outlook.com>
2026-04-22 12:49:19 +0530

    feat: deploy frontend and backend services to EKS with ALB ingress and ACM certificate management

---
+++ infra/80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0106832348EIO1PNV416"
+}
+
+variable "domain_name" {
+  default = "servicewiz.in"
+}
